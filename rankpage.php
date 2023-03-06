551f95064c11487b427ec0cd8bb110b8079bb3c7
jennahenderson023 <125926098+jennahenderson023@users.noreply.github.com>
2023-03-06 18:18:22 +0100

    Add files via upload

---
+++ rankpage.php
@@ -0,0 +1,18 @@
+
+<html>
+
+<head>
+	<h1>Team Expense Rankings</h1>
+</head>
+
+<body>
+	//consumers should navigate here from homepage (it can be linked along with equipment, event, athletes, etc.)
+	<p>1: Football</p>
+	//can we link these back into teams individual expense reports?
+	<p>2: Basketball</p>
+	<p>3: Women's Volleyball</p>
+	<p>4: Baseball</p>
+	<p>5: Soccer</p>
+
+	
+</html>
