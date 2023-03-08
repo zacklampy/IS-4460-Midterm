5be2b4119d095810840ee3c48ce1de75e926dee8
u1309430 <125929814+u1309430@users.noreply.github.com>
2023-03-08 17:10:48 +0100

    Add files via upload

---
+++ equipment.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Utes Football Team Equipment</title>
+	<style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		th, td {
+			text-align: left;
+			padding: 8px;
+			border-bottom: 1px solid #ddd;
+		}
+		th {
+			background-color: #ff0000;
+			color: white;
+		}
+		tr:hover {
+			background-color: #f5f5f5;
+		}
+		.add-btn {
+			background-color: #ff0000;
+			color: white;
+			padding: 12px 20px;
+			border: none;
+			border-radius: 4px;
+			cursor: pointer;
+			float: right;
+		}
+		.add-btn:hover {
+			background-color: #45a049;
+		}
+	</style>
+</head>
+<body>
+	<h1>Utes Football Team Equipment</h1>
+	<table>
+		<tr>
+			<th>Item</th>
+			<th>Size</th>
+			<th>Quantity</th>
+			<th>Status</th>
+			<th>Notes</th>
+		</tr>
+		<tr>
+			<td>Helmets</td>
+			<td>Medium</td>
+			<td>10</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Helmets</td>
+			<td>Large</td>
+			<td>18</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Shoulder Pads</td>
+			<td>Medium</td>
+			<td>12</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Shoulder Pads</td>
+			<td>Large</td>
+			<td>16</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Jerseys</td>
+			<td>Medium</td>
+			<td>15</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Jerseys</td>
+			<td>Large</td>
+			<td>13</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Pants</td>
+			<td>Medium</td>
+			<td>20</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Pants</td>
+			<td>Large</td>
+			<td>8</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Cleats</td>
+			<td>Mens 10-12</td>
+			<td>8</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Cleats</td>
+			<td>Mens 12-15</td>
+			<td>20</td>
+			<td>Good</td>
+			<td>None</td>
+		</tr>
+		<tr>
+			<td>Mouth Guards</td>
+			<td>NA</td>
+			<td>28</td>
+			<td>Good</td>
+			<td>Order More</td>
+		</tr>
+	</table>
+	<button class="add-btn">Add Item</button>
+</body>
+</html>
