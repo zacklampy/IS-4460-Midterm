3edd5ce18f55b64a02c59403911dbc0f33946b02
u1309430 <125929814+u1309430@users.noreply.github.com>
2023-03-08 17:11:34 +0100

    Add files via upload

---
+++ roster.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Utes Football Team Roster</title>
+</head>
+<body>
+	<h1>Utes Football Team Roster</h1>
+	<table>
+		<thead>
+			<tr>
+				<th>Player</th>
+				<th>Position</th>
+				<th>Height</th>
+				<th>Weight</th>
+				<th>Age</th>
+				<th>Scholarship</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Johnny Bravo</td>
+				<td>Quarterback</td>
+				<td>6'2"</td>
+				<td>220 lbs</td>
+				<td>21</td>
+				<td>Walk On</td>
+			</tr>
+			</tr>
+				<td>Ryan O'Rourke</td>
+				<td>Quarterback</td>
+				<td>6'5"</td>
+				<td>225 lbs</td>
+				<td>21</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Jane Smith</td>
+				<td>Quarterback</td>
+				<td>5'9"</td>
+				<td>170 lbs</td>
+				<td>18</td>
+				<td> Preffered Walk On</td>
+			</tr>
+			<tr>
+				<td>Nick Payne</td>
+				<td>Wide Receiver</td>
+				<td>5'8"</td>
+				<td>136 lbs</td>
+				<td>18</td>
+				<td>Partial Scholarship</td>
+			</tr>
+			<tr>
+				<td>Matt Heffner</td>
+				<td>Wide Receiver</td>
+				<td>6'3"</td>
+				<td>200 lbs</td>
+				<td>24</td>
+				<td>Partial Scholarship</td>
+			</tr>
+			<tr>
+				<td>Zack Lampkin</td>
+				<td>Wide Receiver</td>
+				<td>6'2"</td>
+				<td>190 lbs</td>
+				<td>22</td>
+				<td> Full Ride</td>
+			</tr>
+			<tr>
+				<td>Parker Zaharis</td>
+				<td>Wide Receiver</td>
+				<td>5'0"</td>
+				<td>180 lbs</td>
+				<td>20</td>
+				<td> Walk On</td>
+			</tr>
+			<tr>
+				<td>Josh Beard</td>
+				<td>Wide Receiver</td>
+				<td>6'3"</td>
+				<td>185 lbs</td>
+				<td>25</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Garrett Oremus</td>
+				<td>Running Back</td>
+				<td> 6'8"</td>
+				<td>215 lbs</td>
+				<td>21</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>Matt Simmons</td>
+				<td>Running Back</td>
+				<td>5'10"</td>
+				<td>150 lbs</td>
+				<td>25</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>Chad Hines</td>
+				<td>Running Back</td>
+				<td>6'1"</td>
+				<td>175 lbs</td>
+				<td>21</td>
+				<td> Full Ride</td>
+			</tr>
+			<tr>
+				<td>Mike Gordan</td>
+				<td>Running Back</td>
+				<td>5'11"</td>
+				<td>185 lbs</td>
+				<td>23</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Bob Johnson</td>
+				<td>Running Back</td>
+				<td>5'11"</td>
+				<td>205 lbs</td>
+				<td>20</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Chad Hines</td>
+				<td>Running Back</td>
+				<td>6'1"</td>
+				<td>175 lbs</td>
+				<td>21</td>
+				<td>Walk On</td>
+			</tr>
+			<tr>
+				<td>Chong Oh</td>
+				<td>Corner Back</td>
+				<td>6'5"</td>
+				<td>170 lbs</td>
+				<td>19</td>
+				<td>Walk On</td>
+			</tr>
+			<tr>
+				<td>Jacob Case</td>
+				<td>Corner Back</td>
+				<td>6'1"</td>
+				<td>165 lbs</td>
+				<td>20</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>James Jimmy</td>
+				<td>Corner Back</td>
+				<td>6'3"</td>
+				<td>190 lbs</td>
+				<td>21</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>Blakely Boeh</td>
+				<td>Corner Back</td>
+				<td>6'2"</td>
+				<td>180 lbs</td>
+				<td>22</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Jordan Allek</td>
+				<td>Safety</td>
+				<td>6'4"</td>
+				<td>200 lbs</td>
+				<td>21</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Timmy Laps</td>
+				<td>Line Backer</td>
+				<td>6'4"</td>
+				<td>205 lbs</td>
+				<td>22</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Chase Hart</td>
+				<td>Line Backer</td>
+				<td>6'2"</td>
+				<td>200 lbs</td>
+				<td>20</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Gage Buss</td>
+				<td>Offensive Lineman</td>
+				<td>6'4"</td>
+				<td>190 lbs</td>
+				<td>23</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>Cole Rummel</td>
+				<td>Offensive Lineman</td>
+				<td>6'5"</td>
+				<td>210 lbs</td>
+				<td>23</td>
+				<td>Partial Offer</td>
+			</tr>
+			<tr>
+				<td>Zak Shariff</td>
+				<td>Offensive Lineman</td>
+				<td>6'4"</td>
+				<td>205 lbs</td>
+				<td>24</td>
+				<td>Walk On</td>
+			</tr>
+			<tr>
+				<td>Bode Campos</td>
+				<td>Defensive Lineman</td>
+				<td>6'0"</td>
+				<td>220 lbs</td>
+				<td>19</td>
+				<td>Walk On</td>
+			</tr>
+			<tr>
+				<td>Oliver Sho</td>
+				<td>Defensive Lineman</td>
+				<td>6'1"</td>
+				<td>215 lbs</td>
+				<td>20</td>
+				<td>Full Ride</td>
+			</tr>
+			<tr>
+				<td>Jack Duffy</td>
+				<td>Defensive Lineman</td>
+				<td>5'5"</td>
+				<td>220 lbs</td>
+				<td>18</td>
+				<td>Walk On</td>
+			</tr>
+			<tr>
+				<td>Zack Moresco</td>
+				<td>Kicker </td>
+				<td>5'8"</td>
+				<td>150 lbs</td>
+				<td>20</td>
+				<td>Partial Offer</td>
+			</tr>
+		</tbody>
+	</table>
+	<br>
+	<button onclick="location.href='addathlete.php'">Add Athlete</button>
+	<button onclick="location.href='home.php'">Return to Home</button>
+</body>
+</html>
